f95b6e78daf1ea1ad3adedd742053f57b8699288
Daniel Blanco <daniel@blancomartin.cl>
2018-03-18 16:50:33 -0300

    [TEST] added confirm_order function to debug

--- controllers/main.py
+++ controllers/main.py
@@ -147,5 +147,25 @@ class WebpayController(http.Controller):
             values = {
                 'webpay_redirect': resp,
             }
-            return request.website.render('payment_webpay.webpay_redirect', values)
+            return request.website.render(
+                'payment_webpay.webpay_redirect', values)
 
+    @http.route(
+        ['/shop/confirm_order'], type='http', auth="public", website=True)
+    def confirm_order(self, **post):
+        cr, uid, context, registry = request.cr, request.uid, request.context, \
+                                     request.registry
+        order = request.website.sale_get_order(context=context)
+        # if not order:
+        #     return request.redirect("/shop")
+        # redirection = self.checkout_redirection(order)
+        # if redirection:
+        #     return redirection
+        # values = self.checkout_values(post)
+        # values["error"] = self.checkout_form_validate(values["checkout"])
+        # if values["error"]:
+        #     return request.website.render("website_sale.checkout", values)
+        # self.checkout_form_save(values["checkout"])
+        # request.session['sale_last_order_id'] = order.id
+        # request.website.sale_get_order(update_pricelist=True, context=context)
+        return request.redirect("/shop/payment")
